93d28557dba10db22031eda15630e74a8425ac63
IRiDecenT <3360910573@qq.com>
2023-12-03 23:30:48 +0800

    修改demo页面

--- Album/src/api/user.js
+++ Album/src/api/user.js
@@ -4,7 +4,7 @@ import request from '@/utils/request'
 export const userRegister = (registerData) => {
     return request({
         method: 'POST',
-        url: '/register/',
+        url: 'register/',
         headers: {
             'Content-Type': 'application/json'
         },
@@ -15,10 +15,31 @@ export const userRegister = (registerData) => {
 export const userLogin = (loginData) => {
     return request({
         method: 'POST',
-        url: '/login/',
+        url: 'login/',
         headers: {
             'Content-Type': 'application/json'
         },
         data: loginData
+    })
+}
+
+// 此处的uid是用户的id， 通过路由传递过来， js中用``定义模版字符串， ${}为占位符
+export const getUserInfo = (uid) => {
+    return request({
+        method: 'GET',
+        url: `getUserInfo/${uid}/`,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+}
+
+export const getAlbums = (uid) => {
+    return request({
+        method: 'GET',
+        url: `getAlbums/${uid}/`,
+        headers: {
+            'Content-Type': 'application/json'
+        },
     })
 }
